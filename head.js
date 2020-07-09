da047aad8229c8120e5e2b7f984145e198624dbe
xTapG <63505960+xTapG@users.noreply.github.com>
2020-07-09 16:57:09 +0300

    Add files via upload

---
+++ head.js
@@ -0,0 +1,28 @@
+var endpoint = "https://jsonbox.io/box_dataBaseOfShrtUrls";
+
+function fetchJSON(a) {
+    var f = new XMLHttpRequest;
+    f.open("GET", a, false);
+    f.send(null);
+    return f.responseText
+}
+
+function isURL(a) {
+    let url = a
+    if (!a.startsWith("javascript:")) {
+        return true;
+    } else {
+        return false;
+    }
+}
+var hashh = window.location.hash.substr(1);
+if (window.location.hash != "") {
+    var res = JSON.parse(fetchJSON(endpoint + "/?q=s:" + hashh))[0];
+    var data = res["l"];
+    console.log(data);
+    if (data != null) {
+        if (isURL(data)) {
+            window.location.href = data;
+        }
+    }
+}
